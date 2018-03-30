a7b4ab8c5d2b380a5b93a10d0aafea221250bd8b
jemalmoha <jemaltahir83@gmail.com>
2018-03-30 16:52:08 +0300

    the app devided in to different components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./components/App";
-
+import SearchBar from "./components/SearchBar";
+const API_KEY = "AIzaSyBB0iqGuzUsawSxlJTSY7tok1g0XERgQLI";
 const mountNode = document.getElementById("root");
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 render(<App />, mountNode);
